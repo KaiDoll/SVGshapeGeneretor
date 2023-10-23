c18afa5f48fd6ffb467b1e96ca64dda1ded16e65
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-10-23 14:39:20 -0500

    Solved troubleshoot

--- Lib/shape.test.js
+++ Lib/shape.test.js
@@ -1,4 +1,6 @@
-const { Circle } = require("./shapes");
+const { Circle } = require("./shapes"); //not sure what is happening?? 
+const { Square } = require("./shapes");
+const { Triangle } = require("./shapes");
 
 describe("Tests for the circle class", () => {
   test("will a new instance of the circle class return an object?", () => {
@@ -18,6 +20,52 @@ describe("Tests for the circle class", () => {
     expect(circle.textcolor).toBe("red");
     expect(circle.color).toBe("a");
     expect(circle.text).toBe("b");
+
+  });
+  
+});
+
+describe("Tests for the square class", () => {
+  test("will a new instance of the square class return an object?", () => {
+    const square = new Square();
+
+    expect(typeof square).toBe("object");
+  });
+
+  test("Will the new instance of the Square class return the svg string", () => {
+    const square = new Square("green", "LOT", "white");
+
+    expect(square.render()).toBe(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect x="90" y="40" width="120" height="120" fill="green" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">LOT</text></svg>`);
+  });
+
+  test("if we create a new instance of square will it return a text color", () => {
+    const square = new Square("c", "d", "pink");
+    expect(square.textcolor).toBe("pink");
+    expect(square.color).toBe("c");
+    expect(square.text).toBe("d");
+
+  });
+  
+});
+
+describe("Tests for the triangle class", () => {
+  test("will a new instance of the triangle class return an object?", () => {
+    const triangle = new Triangle();
+
+    expect(typeof triangle).toBe("object");
+  });
+
+  test("Will the new instance of the Triangle class return the svg string", () => {
+    const triangle = new Triangle("yellow", "BLW", "white");
+
+    expect(triangle.render()).toBe(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150,20 280,180 20,180" fill="yellow" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">BLW</text></svg>`);
+  });
+
+  test("if we create a new instance of square will it return a text color", () => {
+    const triangle = new Triangle("e", "f", "purple");
+    expect(triangle.textcolor).toBe("purple");
+    expect(triangle.color).toBe("e");
+    expect(triangle.text).toBe("f");
 
   });
   
--- Lib/shapes.js
+++ Lib/shapes.js
@@ -14,19 +14,13 @@ class Circle extends Shape {
 
 class Square extends Shape {
   render() {
-    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        <rect x="90" y="40" width="120" height="120" fill="${this.color}" />
-        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textcolor}">${this.text}</text>
-</svg>`
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect x="90" y="40" width="120" height="120" fill="${this.color}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textcolor}">${this.text}</text></svg>`
   }
 };
 
 class Triangle extends Shape {
   render() {
-    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        <polygon points="150,20 280,180 20,180" fill="${this.color}" />
-        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textcolor}">${this.text}</text>
-</svg>`
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150,20 280,180 20,180" fill="${this.color}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textcolor}">${this.text}</text></svg>`
   }
 };
 
